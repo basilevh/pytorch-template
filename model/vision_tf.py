6b4b8aa748eaecb7da51523a35c33bb523151a14
Basile VH <bvh@outlook.be>
2022-11-28 12:31:20 -0500

    modernize vision tf

--- model/vision_tf.py
+++ model/vision_tf.py
@@ -18,7 +18,7 @@ import timm
 from timesformer.models.vit import TimeSformer
 
 
-# NOTE: Not used in my augs, BUT used in most pretrained models.
+# NOTE: Not used in my augs, BUT used in most pretrained models (I think??).
 # https://github.com/rwightman/pytorch-image-models/blob/master/timm/data/constants.py
 IMAGENET_DEFAULT_MEAN = (0.485, 0.456, 0.406)
 IMAGENET_DEFAULT_STD = (0.229, 0.224, 0.225)
@@ -114,7 +114,8 @@ class DenseTimeSformer(torch.nn.Module):
     '''
 
     def __init__(self, logger, pretrained_frozen, pretrained_path, frame_height, frame_width,
-                 patch_dim, in_channels, num_frames, attention_type):
+                 patch_dim, in_channels, num_frames, attention_type, causal_attention,
+                 norm_embeddings, drop_path_rate, network_depth):
         super().__init__()
         self.logger = logger
         self.pretrained = pretrained_frozen
@@ -132,43 +133,47 @@ class DenseTimeSformer(torch.nn.Module):
         # Extra options.
         self.T = num_frames
         self.attention_type = attention_type
+        self.causal_attention = causal_attention
+        self.norm_embeddings = norm_embeddings
+        self.drop_path_rate = drop_path_rate
+        self.network_depth = network_depth
 
         self.timesformer = TimeSformer(
-            img_size=(self.Hf, self.Wf), patch_size=16, num_classes=0, num_frames=self.T,
-            attention_type=self.attention_type, in_chans=self.Ci,
-            pretrained_model=pretrained_path)
-        assert self.ho == 16 and self.wo == 16
-        self.output_feature_dim = 768
-
-        if pretrained_frozen:
-            # Disable gradients for target features.
-            # NOTE: used_model must always be set to eval, regardless of this model phase.
-            for param in self.timesformer.parameters():
-                param.requires_grad_(False)
+            img_size=(self.Hf, self.Wf), patch_size=patch_dim, num_classes=0, num_frames=self.T,
+            attention_type=self.attention_type, causal_attention=self.causal_attention,
+            drop_path_rate=self.drop_path_rate, network_depth=network_depth,
+            pretrained=self.pretrained, pretrained_model=pretrained_path, in_chans=self.Ci)
+        self.output_feature_dim = self.timesformer.model.embed_dim  # Typically 768 or 1024.
 
         # Taken from their dataset code (Kinetics and SSv2):
-        self.data_mean = [0.45, 0.45, 0.45]
-        self.data_std = [0.225, 0.225, 0.225]
+        # self.data_mean = [0.45, 0.45, 0.45]
+        # self.data_std = [0.225, 0.225, 0.225]
 
     def forward(self, input_pixels):
         '''
         :param input_pixels (B, C, T, H, W) tensor.
+        :return output_features or (output_features, extra_token_out).
+            output_features (B, D, T, H, W) tensor.
+            extra_token_out (B, D, N) tensor.
         '''
 
         # Normalize if pretrained.
+        # https://github.com/facebookresearch/TimeSformer/issues/10
+        # NOTE: If there are more than 3 channels, we assume the first 3 are RGB and leave the rest
+        # untouched.
         if self.pretrained:
             mean = torch.tensor(TIMESFORMER_MEAN, dtype=input_pixels.dtype,
                                 device=input_pixels.device)
-            mean = mean[:, None, None, None].expand_as(input_pixels[0])
+            mean = mean[:, None, None, None].expand_as(input_pixels[0, 0:3])
             std = torch.tensor(TIMESFORMER_STD, dtype=input_pixels.dtype,
                                device=input_pixels.device)
-            std = std[:, None, None, None].expand_as(input_pixels[0])
-            input_pixels = input_pixels - mean
-            input_pixels = input_pixels / std
+            std = std[:, None, None, None].expand_as(input_pixels[0, 0:3])
+            input_pixels[:, 0:3] = input_pixels[:, 0:3] - mean[None]
+            input_pixels[:, 0:3] = input_pixels[:, 0:3] / std[None]
 
         # Adapted from
         # https://github.com/facebookresearch/TimeSformer/blob/main/timesformer/models/vit.py
-        # See third_party/timesformer/... for actual code.
+        # See fr-timesformer (or Ctrl+Click TimeSformer) for actual code.
         B = input_pixels.shape[0]
         x, T, W = self.timesformer.model.patch_embed(input_pixels)
         assert T == self.T
@@ -226,19 +231,28 @@ class DenseTimeSformer(torch.nn.Module):
         x = torch.cat((cls_tokens, x), dim=1)
 
         # Attention blocks
+        y = x
         for blk in self.timesformer.model.blocks:
-            x = blk(x, B, T, W)
+            y = blk(y, B, T, W)
 
-        # Discard cls_token altogether, and skip norm, head.
-        x = x[:, 1:]
+        # Layer normalization is traditionally applied for all tokens.
+        if self.norm_embeddings:
+            y = self.timesformer.model.norm(y)
 
-        x = rearrange(x, 'B (H W T) D -> B D T H W',
+        # Separate output corresponding to cls / extra input token position.
+        extra_token_out = y[:, 0, :].unsqueeze(-1)  # (B, D, 1).
+
+        # Discard cls_token altogether.
+        # Skip head (traditionally for cls only).
+        y = y[:, 1:]
+
+        y = rearrange(y, 'B (H W T) D -> B D T H W',
                       B=B, T=T, H=self.Ho, W=self.Wo, D=self.output_feature_dim)
-        output_features = x
+        output_features = y  # (B, D, T, H, W).
 
         assert output_features.shape[1] == self.output_feature_dim
 
-        return output_features
+        return (output_features, extra_token_out)
 
 
 class MyDenseVisionTransformerBackbone(DenseVisionTransformer):
@@ -257,41 +271,33 @@ class MyDenseTimeSformerBackbone(DenseTimeSformer):
     Trainable variant of the DenseTimeSformerBackbone.
     '''
 
-    def __init__(self, logger, num_frames=16, frame_height=224, frame_width=224,
-                 in_channels=3, attention_type='divided_space_time'):
-        super().__init__(logger, False, '', frame_height, frame_width, 16, in_channels, num_frames,
-                         attention_type)
+    def __init__(self, logger, num_frames=16, frame_height=224, frame_width=288,
+                 patch_dim=16, in_channels=3, pretrained=False, pretrained_path='',
+                 attention_type='divided_space_time', causal_attention=False, norm_embeddings=False,
+                 drop_path_rate=0.1, network_depth=12):
+        super().__init__(
+            logger, pretrained, pretrained_path, frame_height, frame_width, patch_dim, in_channels, num_frames,
+            attention_type, causal_attention, norm_embeddings, drop_path_rate, network_depth)
 
 
 if __name__ == '__main__':
 
     (B, T, H, W, C) = (2, 18, 192, 160, 3)
-    
-    print('MyDenseVisionTransformerBackbone')
-    my_vit = MyDenseVisionTransformerBackbone(None, H, W, C)
-    
-    x = torch.randn(B, C, H, W)
-    print('x:', x.shape, x.min().item(), x.mean().item(), x.max().item())
-    
-    y = my_vit(x)
-    print('y:', y.shape, y.min().item(), y.mean().item(), y.max().item())
-    print()
-    
-    assert y.shape == (B, my_vit.output_feature_dim, H // 16, W // 16)
-    
+    patch_size = 16
+
     for attention_type in ['divided_space_time', 'joint_space_time']:
-        
+
         print('MyDenseTimeSformerBackbone')
         print('attention_type:', attention_type)
-        my_tsf = MyDenseTimeSformerBackbone(None, T, H, W, C, attention_type)
-        
+        my_tsf = MyDenseTimeSformerBackbone(None, T, H, W, patch_size, C, attention_type)
+
         x = torch.randn(B, C, T, H, W)
         print('x:', x.shape, x.min().item(), x.mean().item(), x.max().item())
-        
-        y = my_tsf(x)
+
+        y = my_tsf(x, None)
         print('y:', y.shape, y.min().item(), y.mean().item(), y.max().item())
         print()
-        
-        assert y.shape == (B, my_tsf.output_feature_dim, T, H // 16, W // 16)
+
+        assert y.shape == (B, my_tsf.output_feature_dim, T, H // patch_size, W // patch_size)
 
     pass
